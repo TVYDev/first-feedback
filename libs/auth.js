b80db8216e728ec9dcd413a9beefbd84ed29e49e
Vannyou <vannyoutang23@gmail.com>
2021-06-16 21:07:54 +0700

    chore: add users to firestore

--- libs/auth.js
+++ libs/auth.js
@@ -1,47 +1,60 @@
-import React, {createContext, useContext, useEffect, useState} from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { createUser } from './db';
 import firebase from './firebase';
 
 const authContext = createContext();
 
 export function AuthProvider({ children }) {
-    const auth = useProviderAuth();
-    return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+  const auth = useProviderAuth();
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
 }
 
 export const useAuth = () => {
-    return useContext(authContext);
-}
+  return useContext(authContext);
+};
 
 function useProviderAuth() {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    const signInWithGithub = async () => {
-        const { user } = await firebase
-            .auth()
-            .signInWithPopup(new firebase.auth.GithubAuthProvider());
+  const signInWithGithub = async () => {
+    const { user } = await firebase
+      .auth()
+      .signInWithPopup(new firebase.auth.GithubAuthProvider());
 
-        setUser(user);
-    }
+    const formattedUser = formatUser(user);
+    setUser(formattedUser);
+    createUser(formattedUser.uid, formattedUser);
+  };
 
-    const signOut = async () => {
-        await firebase
-            .auth()
-            .signOut();
+  const signOut = async () => {
+    await firebase.auth().signOut();
 
-        setUser(false);
-    }
+    setUser(false);
+  };
 
-    useEffect(() => {
-        const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
-            setUser(user ?? false)
-        })
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      setUser(formatUser(user) ?? false);
+    });
 
-        return () => unsubscribe();
-    }, []);
+    return () => unsubscribe();
+  }, []);
 
-    return {
-        user,
-        signInWithGithub,
-        signOut
-    }
+  return {
+    user,
+    signInWithGithub,
+    signOut,
+  };
+}
+
+function formatUser(user) {
+  if (!user) return user;
+  // console.log(user.providerData[0].providerId);
+  return {
+    uid: user.uid,
+    name: user.displayName,
+    email: user.email,
+    photoUrl: user.photoURL,
+    providerId: user.providerData[0].providerId,
+  };
 }
